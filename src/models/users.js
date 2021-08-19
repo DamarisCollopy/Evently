b3d0d49d95315adb9c117145856552125c3be022
Damaris-PC <damisoliveira@gmail.com>
2021-08-19 14:41:10 -0300

    inicio

--- src/models/users.js
+++ src/models/users.js
@@ -1,16 +1,50 @@
 const connectionTable = require('../data/database')
 
 class Users {
-    create(users) {
+    
+    create(users, res) {
         const sql = 'INSERT INTO Users SET ?'
 
-        connectionTable.query(sql, users, (erro, result) => {
-            if(erro){
-                console.log(erro)
-            } else {
-                console.log(result)
+        const nameValid = users.name.length === 0
+        const vall = [
+            {
+                name: 'name', 
+                valid : nameValid,
+                mensage: 'Nome invalido'
+            
+            }
+        ] 
+        
+        const err = vall.filter(campo => campo.valid)
+        const existErr = err.length
+        
+        if(existErr){
+            res.status(400).json(err)
+        } else {
+            connectionTable.query(sql, users, (erro, result) => {
+                if(erro){
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(result)
+                }
+            })
+        }
+       
+    }
+
+    lista(res) {
+        const sql = 'SELECT * FROM Users'
+
+        connectionTable.query(sql, (erro, resultado) => { 
+            if(erro) {
+                res.status(400).json(erro)
+            } else { 
+                res.status(201).json(resultado)
             }
         })
     }
+
 }
+
+
 module.exports = new Users
